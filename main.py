3fa6194252b57cb8342e10a700d31bb021ecaa5d
Sadman Sakib <sakibsadman035@gmail.com>
2022-06-25 02:02:43 +0600

    Removed weebhook

--- main.py
+++ main.py
@@ -4,13 +4,10 @@ import os
 from dotenv import load_dotenv
 from subtitle_dw_bot import get_subtitle
 
-from flask import Flask, request
-
 load_dotenv()
 bot_api_key = os.environ.get("BOT_API_KEY")
 
 bot = TeleBot(bot_api_key, parse_mode='HTML')
-server = Flask(__name__)
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -33,22 +30,3 @@ def subtitle(message):
     except Exception as e:
         print(e)
         bot.send_message(message.chat.id, 'Try again.')
-
-
-@server.route('/' + bot_api_key, methods=['POST'])
-def getMessage():
-    json_string = request.get_data().decode('utf-8')
-    update = types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "!", 200
-
-
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://subtitle-downloader-bot.herokuapp.com/' + bot_api_key)
-    return "!", 200
-
-
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
